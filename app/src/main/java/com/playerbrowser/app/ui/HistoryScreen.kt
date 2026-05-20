56be8f8716627bb160de964d46b54c560a7f6651
김상우 <woo8318@gmail.com>
2026-05-20 11:58:20 +0900

    fix: import remember in HistoryScreen

--- app/src/main/java/com/playerbrowser/app/ui/HistoryScreen.kt
+++ app/src/main/java/com/playerbrowser/app/ui/HistoryScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
